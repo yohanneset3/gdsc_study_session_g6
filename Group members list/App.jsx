bcd163b01df5e4368f78a60d844480d6fd6d3b6d
yohanneset3 <149348289+yohanneset3@users.noreply.github.com>
2023-12-12 22:46:42 +0300

    App.jsx

---
+++ Group members list/App.jsx
@@ -0,0 +1,14 @@
+import Members from "./Members";
+function App(){
+  return (
+    <>
+    <div>
+      <h1>Name Of Group Members</h1>
+      <Members/>
+    </div>
+
+    </>
+  )
+}
+
+export default App;
